01f5c6764fb6978daeedbf2e1e001b6cd3171bf3
Jhon avila <jhon.avila@ceiba.com.co>
2021-02-09 11:40:04 -0500

    Se arregla test

--- microservicio/infraestructura/src/test/java/com/ceiba/producto/servicio/testdatabuilder/ComandoProductoTestDataBuilder.java
+++ microservicio/infraestructura/src/test/java/com/ceiba/producto/servicio/testdatabuilder/ComandoProductoTestDataBuilder.java
@@ -9,11 +9,13 @@ public class ComandoProductoTestDataBuilder {
     private Long id;
     private String nombre;
     private String descripcion;
+    private String imagen;
     private Double precio;
 
     public ComandoProductoTestDataBuilder() {
         nombre = UUID.randomUUID().toString();
         descripcion = "1234";
+        imagen = "https://as1.ftcdn.net/jpg/02/15/71/58/500_F_215715894_U2mmLeqcySfnDAnjKvLc03neRkL9DoSX.jpg";
         precio = 10000.00;
     }
 
@@ -23,6 +25,6 @@ public class ComandoProductoTestDataBuilder {
     }
 
     public ComandoProducto build() {
-        return new ComandoProducto(id,nombre, descripcion,precio);
+        return new ComandoProducto(id,nombre, descripcion,imagen, precio);
     }
 }
